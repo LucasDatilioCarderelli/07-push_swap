d3bc87215d58c266c2e3dbadc47aa18750b2fc9d
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2022-03-12 21:47:49 +0100

    create header

---
+++ includes/push_swap.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/12 21:44:07 by ldatilio          #+#    #+#             */
+/*   Updated: 2022/03/12 21:44:47 by ldatilio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../libs/libft/libft.h"
+
+#endif
