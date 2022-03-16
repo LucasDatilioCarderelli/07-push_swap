368ea59d9f23e5f93b58b3a9d54f9677483088b0
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2022-03-17 00:59:39 +0100

    include linked list prototypes

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,16 +6,17 @@
 /*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 21:44:07 by ldatilio          #+#    #+#             */
-/*   Updated: 2022/03/14 02:57:28 by ldatilio         ###   ########.fr       */
+/*   Updated: 2022/03/17 00:58:47 by ldatilio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "../libs/libft/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include "../libs/libft/libft.h"
+# include "../libs/printf/ft_printf.h"
 
 typedef enum e_bool
 {
@@ -23,6 +24,18 @@ typedef enum e_bool
 	FALSE	=	0
 }	t_bool;
 
-int	valid_args(int argc, char **argv);
+typedef struct s_node
+{
+	int				value;
+	struct s_node	*next;
+	struct s_node	*prev;
+}	t_node;
+
+int		valid_args(int argc, char **argv);
+
+void	insert_back(t_node **head, int value);
+void	insert_front(t_node	**head, int value);
+void	free_node(t_node *head);
+void	print_node(t_node *head);
 
 #endif
