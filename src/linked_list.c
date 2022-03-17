bc20b5f984a84fd5b9e68671f6b31fb57e8ec5cf
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2022-03-17 01:01:12 +0100

    add linked list functions

---
+++ src/linked_list.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   linked_list.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/17 00:49:54 by ldatilio          #+#    #+#             */
+/*   Updated: 2022/03/17 00:58:07 by ldatilio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/**
+ * @brief Insert a new_node at the end of the list.
+ * 
+ * @param head First pointer of the list.
+ * @param value Int value of the node.
+ */
+void	insert_back(t_node **head, int value)
+{
+	t_node	*new_node;
+	t_node	*temp;
+
+	new_node = (t_node *)malloc(sizeof(t_node));
+	new_node -> value = value;
+	new_node -> next = NULL;
+	if (*head == NULL)
+	{
+		new_node -> prev = NULL;
+		*head = new_node;
+		return ;
+	}
+	temp = *head;
+	while (temp -> next != NULL)
+		temp = temp -> next;
+	temp -> next = new_node;
+	new_node -> prev = temp;
+}
+
+/**
+ * @brief Insert a new_node at the beginning of the list.
+ * 
+ * @param head First pointer of the list.
+ * @param value Int value of the node.
+ */
+void	insert_front(t_node	**head, int value)
+{
+	t_node	*new_node;
+
+	new_node = (t_node *)malloc(sizeof(t_node));
+	new_node -> value = value;
+	new_node -> next = *head;
+	new_node -> prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = new_node;
+	*head = new_node;
+}
+
+/**
+ * @brief Free the memory allocated for each node in the list.
+ * 
+ * @param head First pointer of the list.
+ */
+void	free_node(t_node *head)
+{
+	t_node	*tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head -> next;
+		free(tmp);
+	}
+}
+
+/**
+ * @brief Print in the output each node from the list;
+ * 
+ * @param head First pointer of the list.
+ */
+void	print_node(t_node *head)
+{
+	while (head != NULL)
+	{
+		ft_printf("%d \n", head -> value);
+		head = head -> next;
+	}
+	if (head == NULL)
+		printf("NULL\n");
+}
--- src/main.c
+++ src/main.c
@@ -6,48 +6,28 @@
 /*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 15:16:05 by ldatilio          #+#    #+#             */
-/*   Updated: 2022/03/12 22:32:24 by ldatilio         ###   ########.fr       */
+/*   Updated: 2022/03/17 00:50:28 by ldatilio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-/**
- * @brief	In case of error, it must display "Error" followed by a ’\\n’
- * 			on the standard error. Print a message and exit with a failure.
- * 
- * @param	error_msg	char string message.
- */
-void	print_error(char *error_msg)
+void	create_stack(int argc, char **argv)
 {
-	write(2, "\033[0;31mError:\033[0m\n", 19);
-	ft_putstr_fd(error_msg, 2);
-	write(2, "\n", 2);
-	exit(EXIT_FAILURE);
-}
-
-/**
- * @brief	not duplicate, only number.
- */
-int	valid_args(int argc, char **argv)
-{
-	int	i;
-	int j;
+	int		i;
+	t_node	*steak_a;
 
 	i = 1;
+	steak_a = NULL;
 	while (i < argc)
 	{
-		j = i + 1;
-		while (j < argc)
-		{
-			printf("i: %d, j: %d\n", ft_atoi(argv[i]), ft_atoi(argv[j]));
-			if (ft_atoi(argv[i]) == ft_atoi(argv[j]))
-				print_error("Found Duplicate");
-			j++;
-		}
+		insert_back(&steak_a, ft_atoi(argv[i]));
 		i++;
 	}
-	return (0);
+	insert_front(&steak_a, 2);
+	print_node(steak_a);
+	print_node(steak_a);
+	free_node(steak_a);
 }
 
 int	main(int argc, char **argv)
@@ -55,5 +35,6 @@ int	main(int argc, char **argv)
 	if (argc < 3)
 		return (1);
 	valid_args(argc, argv);
+	create_stack(argc, argv);
 	return (0);
 }
