867765e684e212b81693331d00c00da8f7b25a52
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2022-03-19 00:24:41 +0100

    add rules prototypes

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 21:44:07 by ldatilio          #+#    #+#             */
-/*   Updated: 2022/03/17 00:58:47 by ldatilio         ###   ########.fr       */
+/*   Updated: 2022/03/17 18:04:28 by ldatilio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,10 @@ void	insert_back(t_node **head, int value);
 void	insert_front(t_node	**head, int value);
 void	free_node(t_node *head);
 void	print_node(t_node *head);
+
+void	push(t_node **dest, t_node **src);
+void	rotate(t_node **head);
+void	rrotate(t_node **head);
+void	swap(t_node **head);
 
 #endif
