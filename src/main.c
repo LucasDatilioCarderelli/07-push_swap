941f40574bcd0e14c9883094624705f4a3afd79f
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2022-03-19 00:26:49 +0100

    update main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 15:16:05 by ldatilio          #+#    #+#             */
-/*   Updated: 2022/03/17 00:50:28 by ldatilio         ###   ########.fr       */
+/*   Updated: 2022/03/19 00:22:53 by ldatilio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,39 @@
 void	create_stack(int argc, char **argv)
 {
 	int		i;
-	t_node	*steak_a;
+	t_node	*stack_a;
+	t_node	*stack_b;
 
 	i = 1;
-	steak_a = NULL;
+	stack_a = NULL;
+	stack_b = NULL;
 	while (i < argc)
 	{
-		insert_back(&steak_a, ft_atoi(argv[i]));
+		insert_back(&stack_a, ft_atoi(argv[i]));
 		i++;
 	}
-	insert_front(&steak_a, 2);
-	print_node(steak_a);
-	print_node(steak_a);
-	free_node(steak_a);
+	ft_printf("\nstack_a:\n");
+	print_node(stack_a);
+	ft_printf("stack_b:\n");
+	print_node(stack_b);
+	swap(&stack_a);
+	push(&stack_b, &stack_a);
+	push(&stack_b, &stack_a);
+	push(&stack_b, &stack_a);
+	swap(&stack_a);
+	push(&stack_a, &stack_b);
+	push(&stack_a, &stack_b);
+	push(&stack_a, &stack_b);
+	ft_printf("\nstack_a:\n");
+	print_node(stack_a);
+	ft_printf("stack_b:\n");
+	print_node(stack_b);
+	free_node(stack_a);
+	free_node(stack_b);
 }
 
 int	main(int argc, char **argv)
-{
+{	
 	if (argc < 3)
 		return (1);
 	valid_args(argc, argv);
